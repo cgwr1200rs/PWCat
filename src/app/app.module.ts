172b10b856ee94a71dd222d297459f6063f7a9c1
cgwr1200rs <cornelius.wolf@sydecon.de>
2018-05-02 21:36:45 +0200

    Progressified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import {
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +22,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
     BrowserModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
